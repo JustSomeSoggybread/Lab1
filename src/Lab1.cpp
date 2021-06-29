3a24ef963c5bcb0032c674459f30755e751edd15
JustSomeSoggybread <evanhlee@outlook.com>
2021-06-29 09:02:58 -0700

    first commit

---
+++ src/Lab1.cpp
@@ -0,0 +1,30 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/evanh/OneDrive/Desktop/CTD/InternetOfThings/Lab1/Lab1/src/Lab1.ino"
+/*
+ * Project Lab1
+ * Description:
+ * Author:
+ * Date:
+ */
+
+void setup();
+void loop();
+#line 8 "c:/Users/evanh/OneDrive/Desktop/CTD/InternetOfThings/Lab1/Lab1/src/Lab1.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
